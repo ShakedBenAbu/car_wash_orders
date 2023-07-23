a40ccd8beec130b7cf941c838165efa3d01b5ab9
Shaked Ben Abu <shaked63@gmail.com>
2023-07-23 10:33:05 +0300

    delete fun work

--- functions.py
+++ functions.py
@@ -18,11 +18,25 @@ def add_customer(name, phone, discount,filename = "orders.sqlite"):
         return "Customer added"
 
 
-name = input("Enter the customer's name: ")
-phone = input("Enter the customer's phone number: ")
-discount = float(input("Enter the customer's discount: "))
+# name = input("Enter the customer's name: ")
+# phone = input("Enter the customer's phone number: ")
+# discount = float(input("Enter the customer's discount: "))
 
 
 
-result = add_customer(name, phone, discount)
+# result = add_customer(name, phone, discount)
+# print(result)
+
+
+def delete_customer(customer_id, filename="orders.sqlite"):
+    with sqlite3.connect(filename) as conn:
+        cur = conn.cursor()
+        sql_query = ("DELETE FROM customers WHERE customer_id = ?")
+        cur.execute(sql_query, (customer_id,))
+        conn.commit()
+        return "Customer deleted"
+
+customer_id = int(input("Enter the customer's ID to delete: "))
+
+result = delete_customer(customer_id)
 print(result)
